4dc75338bec6d65efbbd1ff8abcf8ee0c53a5c20
Stefan Lehmann <stlm@posteo.de>
2020-01-08 19:11:27 +0100

    Restructure project

--- src/joyit_flaskapp/flaskapp/__init__.py
+++ src/joyit_flaskapp/flaskapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_redis import FlaskRedis
 from .config import config
 
@@ -6,15 +6,15 @@ from .config import config
 redis_client = FlaskRedis()
 
 
-def create_app(config_name: str="default") -> Flask:
+def create_app(config_name: str = "default") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
     redis_client.init_app(app)
 
     from . import main, redis
+
     app.register_blueprint(main.bp)
     app.register_blueprint(redis.bp)
 
     return app
-
