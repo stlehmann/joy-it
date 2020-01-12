478f73461143051928643a4e2416032cc60a0e25
Stefan Lehmann <stlm@posteo.de>
2020-01-12 11:14:47 +0100

    Add install requirements
    * paho-mqtt
    * flask
    * flask-mqtt

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='joyit',
-    version='0.1.0',
-    packages=['joyit'],
-    package_dir={'': 'src'},
-    url='',
-    license='',
-    author='Stefan Lehmann',
-    author_email='stlm@posteo.de',
-    description=''
+    name="joyit",
+    version="0.1.0",
+    packages=["joyit"],
+    package_dir={"": "src"},
+    url="",
+    license="",
+    author="Stefan Lehmann",
+    author_email="stlm@posteo.de",
+    description="",
+    install_requires=["paho-mqtt", "flask", "flask-mqtt"],
 )
